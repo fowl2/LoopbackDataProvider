894b951853638db77afbe3a19fd6da39a7846b15
James May <fowl2@users.noreply.github.com>
2021-08-18 18:32:00 +1000

    Add project files.

---
+++ LoopbackDataProviderPlugin.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Data.Exceptions;
+using Microsoft.Xrm.Sdk.Extensions;
+using Microsoft.Xrm.Sdk.Query;
+
+using System;
+using System.Linq;
+
+public sealed class LoopbackDataProviderPlugin : IPlugin
+{
+
+
+}
