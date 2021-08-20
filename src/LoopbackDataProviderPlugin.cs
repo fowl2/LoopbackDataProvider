d37702981365f03a5e2b645d03fbcc07a8a46b84
James May <fowl2@users.noreply.github.com>
2021-08-20 13:29:17 +1000

    WIP

---
+++ src/LoopbackDataProviderPlugin.cs
@@ -0,0 +1,130 @@
+﻿
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Data.Exceptions;
+using Microsoft.Xrm.Sdk.Data.Extensions;
+using Microsoft.Xrm.Sdk.Data.Mappings;
+using Microsoft.Xrm.Sdk.Extensions;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LoopbackDataProvider
+{
+    public sealed class LoopbackDataProviderPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.Get<IPluginExecutionContext>()
+                ?? throw new ArgumentException($"Get<{nameof(IPluginExecutionContext)}>() returned null");
+            var service = serviceProvider.GetOrganizationService(Guid.Empty)
+                ?? throw new ArgumentException($"GetOrganizationService() returned null");
+            var tracing = serviceProvider.Get<ITracingService>()
+                ?? throw new ArgumentException($"Get<{nameof(ITracingService)}>() returned null");
+
+            tracing.Trace($"Rev: {GitVersionInformation.InformationalVersion}");
+
+            foreach (var item in context.InputParameters)
+                tracing.Trace($"IP: {item.Key}: {item.Value} ({item.Value?.GetType()})");
+
+            foreach (var item in context.OutputParameters)
+                tracing.Trace($"OP: {item.Key}: {item.Value} ({item.Value?.GetType()})");
+
+            foreach (var item in context.SharedVariables)
+                tracing.Trace($"SV: {item.Key}: {item.Value} ({item.Value?.GetType()})");
+
+
+            var retriever = serviceProvider.Get<IEntityDataSourceRetrieverService>()
+                   ?? throw new ArgumentException($"Get<{nameof(IEntityDataSourceRetrieverService)}>() returned null");
+            var dataSource = retriever.RetrieveEntityDataSource()
+                ?? throw new ArgumentException("RetrieveEntityDataSource() returned null");
+
+            var targetEntityName = dataSource.Attributes.First(x => x.Value is string).Value.ToString();
+
+
+            var mapping = Mapping.Create(service, context.PrimaryEntityName);
+
+            var entityMetadata = service.GetEntityMetadata(context.PrimaryEntityName);
+
+            var typeMapFactory = new DefaultTypeMapFactory();
+
+
+            switch (context.MessageName)
+            {
+                case "Retrieve":
+                    var entityMap =EntityMapFactory.Create(entityMetadata, typeMapFactory, "mappingEntityAlias");
+                    
+                    var target = (EntityReference)context.InputParameters["Target"];
+                    var columnSet = (ColumnSet)context.InputParameters["ColumnSet"];
+                    var relatedEntitiesQuery = (RelationshipQueryCollection)context.InputParameters["RelatedEntitiesQuery"];
+                    if (relatedEntitiesQuery.Any())
+                        throw new NotImplementedException("RelatedEntitiesQuery");
+                    
+                    var mappedTarget = Mapping.ReplaceEntityLogicalNames(target, context.PrimaryEntityName, entityMetadata.ExternalName);
+                    var mappedColumns = columnSet.Columns.Select(entityMap.MapAttributeNameExternal).ToArray();
+
+                    var retrieveResponse = (RetrieveResponse)service.Execute(new RetrieveRequest { 
+                        
+                        ColumnSet = new (mappedColumns),
+                        Target = mappedTarget,
+                    });
+
+                    var mappedEntity = retrieveResponse.Entity.ToEntity(entityMap);
+                    context.OutputParameters["Entity"] = mappedEntity;
+                    return;
+                    
+                case "RetrieveMultiple":
+                    
+                    var queryMapFactory = new QueryMapFactory(service, typeMapFactory);
+
+                    var query = (QueryExpression)context.InputParameters["Query"];
+                    var queryMap = queryMapFactory.Create(query);
+                    
+                    var convertedQuery = query.ConvertSchema(queryMap);
+
+                    tracing.Trace($"{nameof(convertedQuery)}: {convertedQuery.EntityName} ({string.Join(", ", convertedQuery.ColumnSet.Columns)})");
+                    tracing.Trace($"{nameof(convertedQuery)}: Conditions: {convertedQuery.Criteria.Conditions.Count()}");
+
+                    var retrieveMultipleResponse = service.RetrieveMultiple(convertedQuery);
+                    var mappedEntities = new EntityCollection();
+                     
+                    mappedEntities.Entities.AddRange(retrieveMultipleResponse.Entities.Select(x=> x.ToEntity(queryMap.PrimaryEntityMap)));
+
+                    foreach (var item in mappedEntities.Entities)
+                    {
+                        tracing.Trace($"{item.LogicalName} {item.Id} ({string.Join(",", item.Attributes.Select(a=> $"{a.Key}: {a.Value} ({a.Value?.GetType()?.Name})"))})");
+                    }
+
+                    mappedEntities.MoreRecords = retrieveMultipleResponse.MoreRecords;
+                    mappedEntities.MinActiveRowVersion = retrieveMultipleResponse.MinActiveRowVersion;
+                    mappedEntities.TotalRecordCountLimitExceeded = retrieveMultipleResponse.TotalRecordCountLimitExceeded;
+                    mappedEntities.TotalRecordCount = retrieveMultipleResponse.TotalRecordCount;
+                    mappedEntities.PagingCookie = retrieveMultipleResponse.PagingCookie;
+
+                    context.OutputParameters["BusinessEntityCollection"] = mappedEntities; 
+                    return;
+
+                case "Create":
+                case "Update":
+                case "Delete":
+                default:
+                    break;
+            }
+
+            var request = new OrganizationRequest(context.MessageName);
+
+            request.Parameters.AddRange(
+                mapping.ConvertSchema(context.InputParameters));
+
+            var response = service.Execute(request);
+
+            context.OutputParameters.AddRange(
+                mapping.ConvertSchema(response.Results));
+        }
+
+
+    }
+}
---
+++ src/Mapping.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Extensions;
+using Microsoft.Xrm.Sdk.Data.Mappings;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Data.Extensions;
+using Microsoft.Xrm.Sdk.Messages;
+
+namespace LoopbackDataProvider
+{
+    sealed class Mapping
+    {
+        internal static Mapping Create(IOrganizationService service, string primaryEntityName)
+        {
+            var typeMapFactory = new DefaultTypeMapFactory();
+            var queryMapFactory = new QueryMapFactory(service, typeMapFactory);
+            return new(queryMapFactory);
+        }
+
+        readonly QueryMapFactory queryMapFactory;
+
+        Mapping(QueryMapFactory queryMapFactory)
+        {
+            this.queryMapFactory = queryMapFactory;
+        }
+
+        internal IEnumerable<KeyValuePair<string, object>> ConvertSchema(IEnumerable<KeyValuePair<string, object>> kvps)
+            => kvps.Select(ip => new KeyValuePair<string, object>(ip.Key, ConvertSchema(ip.Value)));
+
+        object ConvertSchema(object obj)
+            => obj switch
+            {
+                EntityReference entityReference
+                    => ConvertSchema(entityReference),
+
+                Entity entity
+                    => ConvertSchema(entity),
+
+                EntityCollection entityCollection
+                    => ConvertSchema(entityCollection),
+
+                AttributeCollection attributeCollection
+                    => ConvertSchema(attributeCollection),
+
+                QueryExpression queryExpression
+                    => ConvertSchema(queryExpression),
+
+                _ => obj,
+            };
+
+        EntityCollection ConvertSchema(EntityCollection entityCollection)
+        {
+            throw new NotImplementedException();
+            //var retval = queryExpression.ConvertSchema(queryMap);
+           // return retval;
+        }
+
+        QueryExpression ConvertSchema(QueryExpression queryExpression)
+        {
+            var queryMap = queryMapFactory.Create(queryExpression);
+            var retval = queryExpression.ConvertSchema(queryMap);
+            return retval;
+        }
+
+        object ConvertSchema(object obj, string logicalName, string newLogicalName)
+            => obj switch
+            {
+                EntityReference entityReference when entityReference.LogicalName == logicalName
+                    => ReplaceEntityLogicalNames(entityReference, logicalName, newLogicalName),
+
+                Entity entity when entity.LogicalName == logicalName
+                    => ReplaceEntityLogicalNames(entity, logicalName, newLogicalName),
+
+                EntityCollection entityCollection when entityCollection.EntityName == logicalName
+                    => ReplaceEntityLogicalNames(entityCollection, logicalName, newLogicalName),
+
+                AttributeCollection attributeCollection
+                    => ReplaceEntityLogicalNames(attributeCollection, logicalName, newLogicalName),
+
+                QueryExpression queryExpression
+                    => ReplaceEntityLogicalName(queryExpression, logicalName, newLogicalName),
+
+                _ => obj,
+            };
+
+
+
+        static QueryExpression ReplaceEntityLogicalName(QueryExpression queryExpression, string logicalName, string newLogicalName)
+        {
+            var retval = new QueryExpression()
+            {
+                ColumnSet = queryExpression.ColumnSet,
+                Criteria = queryExpression.Criteria,
+                EntityName = newLogicalName,
+                ExtensionData = queryExpression.ExtensionData,
+                NoLock = queryExpression.NoLock,
+                PageInfo = queryExpression.PageInfo,
+                QueryHints = queryExpression.QueryHints,
+                SubQueryExpression = queryExpression.SubQueryExpression,
+                Distinct = queryExpression.Distinct,
+                TopCount = queryExpression.TopCount,
+            };
+
+            retval.LinkEntities.AddRange(queryExpression.LinkEntities);
+            retval.Orders.AddRange(queryExpression.Orders);
+
+            return retval;
+        }
+
+
+        public static EntityReference ReplaceEntityLogicalNames(EntityReference entityReference, string logicalName, string newLogicalName)
+            => new()
+            {
+                ExtensionData = entityReference.ExtensionData,
+                Id = entityReference.Id,
+                KeyAttributes = entityReference.KeyAttributes,
+                LogicalName = newLogicalName,
+                Name = entityReference.Name,
+                RowVersion = entityReference.RowVersion,
+            };
+
+        static Entity ReplaceEntityLogicalNames(Entity entityReference, string logicalName, string newLogicalName)
+        {
+            var retval = new Entity()
+            {
+                ExtensionData = entityReference.ExtensionData,
+                Id = entityReference.Id,
+                KeyAttributes = entityReference.KeyAttributes,
+                LogicalName = newLogicalName,
+                Attributes = ReplaceEntityLogicalNames(entityReference.Attributes, logicalName, newLogicalName),
+                RowVersion = entityReference.RowVersion,
+                EntityState = entityReference.EntityState,
+                HasLazyFileAttribute = entityReference.HasLazyFileAttribute,
+                LazyFileAttributeKey = entityReference.LazyFileAttributeKey,
+                LazyFileAttributeValue = entityReference.LazyFileAttributeValue,
+                LazyFileSizeAttributeKey = entityReference.LazyFileSizeAttributeKey,
+                LazyFileSizeAttributeValue = entityReference.LazyFileSizeAttributeValue,
+            };
+
+            retval.RelatedEntities.AddRange(ReplaceEntityLogicalNames(retval.RelatedEntities, logicalName, newLogicalName));
+            retval.FormattedValues.AddRange(retval.FormattedValues);
+            return retval;
+        }
+
+        static IEnumerable<Entity> ReplaceEntityLogicalNames(IEnumerable<Entity> entities, string logicalName, string newLogicalName)
+            => entities.Select(e => ReplaceEntityLogicalNames(e, logicalName, newLogicalName));
+
+        static IEnumerable<KeyValuePair<Relationship, EntityCollection>> ReplaceEntityLogicalNames(RelatedEntityCollection kvps, string logicalName, string newLogicalName)
+            => kvps.Select<KeyValuePair<Relationship, EntityCollection>, KeyValuePair<Relationship, EntityCollection>>(
+                _ => throw new NotImplementedException(nameof(RelatedEntityCollection)));
+
+        static AttributeCollection ReplaceEntityLogicalNames(AttributeCollection attributeCollection, string logicalName, string newLogicalName)
+        {
+            throw new NotImplementedException();
+            var retval = new AttributeCollection();
+         //   retval.AddRange(
+        //        ReplaceEntityLogicalNames(attributeCollection.AsEnumerable(), logicalName, newLogicalName));
+            return retval;
+        }
+
+        private static EntityCollection ReplaceEntityLogicalNames(EntityCollection entityCollection, string logicalName, string newLogicalName)
+        {
+            var retval = new EntityCollection()
+            {
+                EntityName = newLogicalName,
+                ExtensionData = entityCollection.ExtensionData,
+                MinActiveRowVersion = entityCollection.MinActiveRowVersion,
+                MoreRecords = entityCollection.MoreRecords,
+                PagingCookie = entityCollection.PagingCookie,
+                TotalRecordCount = entityCollection.TotalRecordCount,
+                TotalRecordCountLimitExceeded = entityCollection.TotalRecordCountLimitExceeded,
+            };
+
+            retval.Entities.AddRange(ReplaceEntityLogicalNames(entityCollection.Entities.AsEnumerable(), logicalName, newLogicalName));
+            return retval;
+        }
+    }
+}
